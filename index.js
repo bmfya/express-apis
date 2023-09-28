4e8668cafc995e3f74d833882dbf9b58265973d9
joyaaaaa <bb0103@foxmail.com>
2023-09-28 11:28:01 +0800

    优化

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+const express = require('express');
 
-async function initializeAPIs(apisPath, express, utils = {}) {
-  if (!express) {
-    return console.error('express is not defined');
-  }
-
+async function initializeAPIs(apisPath, utils = {},) {
   const router = express.Router();
-
   try {
-    const files = await fs.promises.readdir(apisPath);
+    const files = await fs.readdir(apisPath);
     const jsFiles = files.filter((file) => path.extname(file) === '.js');
-    jsFiles.forEach((jsFile) => handleJsFiles(path.join(apisPath, jsFile), router, utils));
+    for (const jsFile of jsFiles) {
+      try {
+        const handleApi = require(path.join(apisPath, jsFile));
+        const {
+          method = 'get',
+          path: apiPath = '/' + path.basename(path.join(apisPath, jsFile), '.js'),
+          middlewares = [],
+        } = handleApi;
+
+        router[method.toLowerCase()](apiPath, ...middlewares, createApiHandler(handleApi, utils));
+      } catch (err) {
+        console.error('Error handling API file:', err);
+      }
+    }
   } catch (err) {
     console.error('API read failed:', err);
+    return null;
   }
-
   return router;
 }
 
-function handleJsFiles(filePath, router, utils) {
-  try {
-    const handleApi = require(filePath);
-    const { method = 'get', path: apiPath = '/' + path.basename(filePath, '.js'), middleware = [] } = handleApi;
-
-    router[method.toLowerCase()](apiPath, ...middleware, handleApiResponse(handleApi, utils));
-  } catch (err) {
-    console.error('Error handling API file:', err);
-  }
-}
-
-function handleApiResponse(handleApi, utils) {
-  return async function (request, response) {
+function createApiHandler(handleApi, utils) {
+  return async (request, response) => {
     try {
       response.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
       const { query, params, body, method } = request;
       const ctx = { method, query, params, body, request, response, ...utils };
       const data = await (typeof handleApi === 'function' ? handleApi(ctx) : handleApi?.handle?.(ctx) || handleApi);
-
       if (data === null || data === undefined) {
         throw new Error('Internal error');
       }
-
       response.status(200)[typeof data === 'object' && data !== null ? 'json' : 'send'](data);
     } catch (error) {
       response.status(400).json({
